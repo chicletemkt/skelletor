d5a72a19b2e27d8075dd1c2d484526f7646d96f7
Ronaldo Faria Lima <ronaldo.faria.lima@gmail.com>
2017-02-04 11:48:45 -0200

    Ajustes na apresentação da versão na tela Mais Ajustes no splash screen.

---
+++ Sources/Util/BundleVersion.swift
@@ -0,0 +1,26 @@
+//
+//  BundleVersion.swift
+//  Skelletor
+//
+//  Created by Ronaldo Faria Lima on 04/02/17.
+//  Copyright © 2017 Nineteen Apps. All rights reserved.
+//
+
+import Foundation
+
+public struct BundleVersion {
+    let bundle: Bundle
+    public let version: String!
+    public var versionComponents: [String] {
+        return version.components(separatedBy: ".")
+    }
+    
+    public init(with bundle: Bundle) {
+        self.bundle = bundle
+        if let dict = bundle.infoDictionary {
+            version = "\(dict["CFBundleShortVersionString"]!).\(dict["CFBundleVersion"]!)"
+        } else {
+            version = nil
+        }
+    }
+}
