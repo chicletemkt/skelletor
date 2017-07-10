8662c1f7a4a69ffc94f173067f79144aa50b63cf
Ronaldo Faria Lima <ronaldo@nineteen.com.br>
2017-07-10 14:35:05 -0300

    Implementation of fetch requests.

--- Skelletor/Data/WebServiceStore.swift
+++ Skelletor/Data/WebServiceStore.swift
@@ -7,15 +7,22 @@
 //
 
 import CoreData
-
+import Foundation
 
 /// Errors thrown by WebServiceStore methods.
 ///
 /// - noStoreURL: No Store URL was provided
 /// - invalidURLType: It was provided an unsupported URL type for this store
+/// - noEntity: Fetch request has no entity attached to it
+/// - noContext: Managed object context was not provided
+/// - mustBeOverriden: Method must be overriden by descendant classes
 public enum WebServiceStoreError: Error {
     case noStoreURL
     case invalidURLType
+    case invalidRequest
+    case noEntity
+    case noContext
+    case mustBeOverriden
 }
 
 /// General web service store for incremental stores.
@@ -70,5 +77,94 @@ open class WebServiceStore: NSIncrementalStore {
             throw WebServiceStoreError.invalidURLType
         }
         self.metadata = [NSStoreUUIDKey: uuid, NSStoreTypeKey: type]
+    }
+    
+    /// Executes a request against the store
+    ///
+    /// - Parameters:
+    ///   - request: fetch request or save request to be executed.
+    ///   - context: managed object context
+    /// - Returns: List of managed objects, if it is the case.
+    /// - Throws: 
+    ///     - WebServiceStoreError.noContext
+    ///     - WebServiceStoreError.noEntity
+    ///     - WebServiceStoreError.noExecutor(entity)
+    open override func execute(_ request: NSPersistentStoreRequest, with context: NSManagedObjectContext?) throws -> Any {
+        switch request {
+        case let fetchRequest as NSFetchRequest<NSFetchRequestResult>:
+            return try execute(fetchRequest: fetchRequest, with: context)
+        case let saveRequest as NSSaveChangesRequest:
+            return try execute(saveRequest: saveRequest, with: context)
+        default:
+            throw WebServiceStoreError.invalidRequest
+        }
+    }
+    
+    /// Resolves faults by asking the web service about data
+    ///
+    /// - Parameters:
+    ///   - objectID: Object ID
+    ///   - context: Managed context
+    /// - Returns: The obtained node with fault data resolved
+    open override func newValuesForObject(with objectID: NSManagedObjectID, with context: NSManagedObjectContext) throws -> NSIncrementalStoreNode {
+        let objectKey = referenceObject(for: objectID)
+        let entity = objectID.entity.name!
+        let values = try getValuesForObject(for: entity, and: objectKey)
+        return NSIncrementalStoreNode(objectID: objectID, withValues: values, version: 0)
+    }
+}
+
+// MARK: - Extension Points
+extension WebServiceStore {
+    /// Returns unique IDs for object creation.
+    ///
+    /// - Parameter fetchRequest: Fetch request to evaluate
+    /// - Returns: A list of unique ids, grabbed from a web service
+    /// - Important:
+    /// This method must be overriden. Its default implementation just throws an exception.
+    open func getUniqueIds(for fetchRequest: NSFetchRequest<NSFetchRequestResult>) throws -> [Any] {
+        throw WebServiceStoreError.mustBeOverriden
+    }
+    
+    /// Get values from the web service for a given entity and key
+    ///
+    /// - Parameters:
+    ///   - entity: Entity name
+    ///   - key: Entity key information (e.g., primary key)
+    /// - Returns: List of pairs attribute/values
+    /// - Important:
+    /// This method must be overriden. Its default implementation just throws an exception.
+    open func getValuesForObject(for entity: String, and key: Any) throws -> [String:Any] {
+        throw WebServiceStoreError.mustBeOverriden
+    }
+}
+
+// MARK: - Internal Methods
+extension WebServiceStore {
+    func execute(fetchRequest: NSFetchRequest<NSFetchRequestResult>, with context: NSManagedObjectContext?) throws -> [NSManagedObject] {
+        guard let context = context else {
+            throw WebServiceStoreError.noContext
+        }
+        guard let entityName = fetchRequest.entity?.name else {
+            throw WebServiceStoreError.noEntity
+        }
+        var managedObjects = [NSManagedObject]()
+        for key in try getUniqueIds(for: fetchRequest) {
+            if let objectID = newObjectId(for: key, entity: entityName, using: context) {
+                managedObjects.append(context.object(with: objectID))
+            }
+        }
+        return managedObjects
+    }
+    
+    func execute(saveRequest: NSSaveChangesRequest, with context: NSManagedObjectContext?) throws -> [NSManagedObject] {
+        return [NSManagedObject]()
+    }
+    
+    func newObjectId(for key: Any, entity: String, using context: NSManagedObjectContext) -> NSManagedObjectID? {
+        if let entityDescription = NSEntityDescription.entity(forEntityName: entity, in: context) {
+            return newObjectID(for: entityDescription, referenceObject: key)
+        }
+        return nil
     }
 }
