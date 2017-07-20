c922a74e65dea87b824bbf6ab93075653b93e58a
Ronaldo Faria Lima <ronaldo@nineteen.com.br>
2017-07-19 21:25:32 -0300

    Final implementation of store for saving user data.

--- Skelletor/Data/IncrementalWebService.swift
+++ Skelletor/Data/IncrementalWebService.swift
@@ -1,47 +1,129 @@
 //
-//  IncrementalWebService.swift
-//  Skelletor
+//  WebService.swift
+//  Point
 //
-//  Created by Ronaldo Faria Lima on 17/07/17.
-//  Copyright © 2017 Nineteen Apps. All rights reserved.
+//  Created by Ronaldo Faria Lima on 14/07/17.
+//  Copyright © 2017 Point. All rights reserved.
 //
 
 import Foundation
 import CoreData
 
-public protocol IncrementalWebService {
-    /// Returns unique IDs for object creation.
-    ///
-    /// - Parameter fetchRequest: Fetch request to evaluate
-    /// - Returns: A list of unique ids, grabbed from a web service
-    /// - Important:
-    /// This method must be overriden. Its default implementation just throws an exception.
-    func getUniqueIds(for fetchRequest: NSFetchRequest<NSFetchRequestResult>) throws -> [Any]
+/// Errors thrown by the web service
+///
+/// - requestError: Error occurred during a given request.
+/// - operationTimedOut: Operation has timed out
+public enum IncrementalWebServiceError: Error {
+    case requestError(message: String)
+    case operationTimedOut
+    case invalidDataReceived
+    case wrongNumberOfItemsReceived
+    case mustBeOverriden
+}
+
+/// Encapsulates all about a given web service
+open class IncrementalWebService {
+    public weak var store: IncrementalWebServiceStore!
+    public let rootURL: URL
+    public let apiKey: String
+    open var entity: String {
+        return "Entity"
+    }
     
-    /// Get values from the web service for a given entity and key
+    /// Initializer
     ///
     /// - Parameters:
-    ///   - key: Entity key information (e.g., primary key)
-    /// - Returns: List of pairs attribute/values
-    /// - Important:
-    /// This method must be overriden. Its default implementation just throws an exception.
-    func getValuesForObject(using key: Any) throws -> [String:Any]
+    ///   - rootURL: Root service URL
+    ///   - session: URLSession for communications
+    public init(rootURL: URL, apiKey: String, store: IncrementalWebServiceStore) {
+        self.rootURL = rootURL
+        self.apiKey = apiKey
+        self.store = store
+    }
     
-    /// Create permanent object keys for a list of categorized objects
-    ///
-    /// - Parameter objects: List of objects
-    /// - Returns: List of tuples Key/Object
-    /// - Important:
-    /// This method must be overriden. Its default implementation just throws an exception.
-    func createKeys(for objects: [NSManagedObject]) throws -> [(Any, NSManagedObject)]
-    
-    /// Persist data to the backing store
+    /// Builds a request out of an URL, using a closure for proper configuration
     ///
     /// - Parameters:
-    ///   - saveRequest: Save request to execute
-    ///   - context: managed object context
-    /// - Returns: An empty list o NSManagedObject on success
-    /// - Important:
-    /// This method must be overriden. Its default implementation just throws an exception.
-    func persistToBackingStore(saveRequest: NSSaveChangesRequest, with context: NSManagedObjectContext?) throws
+    ///   - from: URL to use for the request
+    ///   - configure: Closure used for request configuration.
+    /// - Returns: Fully configured request, ready to use.
+    public func request(from: URL, configure: ((URLRequest) -> URLRequest)? = nil) -> URLRequest {
+        var request = URLRequest(url: from)
+        request.addValue(apiKey, forHTTPHeaderField: "x-api-key")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        if let configure = configure {
+            request = configure(request)
+        }
+        return request
+    }
+    
+    /// Executes a given request, synchronously.
+    ///
+    /// - Parameters:
+    ///   - request: Request to execute
+    ///   - timeout: Timeout in seconds. Defaults to 30s
+    ///   - callback: Callback that receives data returned from the server
+    /// - Throws: PointWebServiceStoreError.downloadError,
+    ///           PointWebServiceStoreError.operationTimedOut
+    public func execute(request: URLRequest, timeout: TimeInterval = 30, callback: ((Data?) throws -> Void)? = nil) throws {
+        let barrier = NSCondition()
+        barrier.lock()
+        defer {
+            barrier.unlock()
+        }
+        var foundError: IncrementalWebServiceError?
+        let task = store.session.dataTask(with: request) { (data, response, error) in
+            if error != nil, error?.localizedDescription != nil {
+                foundError = IncrementalWebServiceError.requestError(message: error!.localizedDescription)
+            } else {
+                do {
+                    try callback?(data)
+                } catch {
+                    foundError = IncrementalWebServiceError.invalidDataReceived
+                }
+            }
+            barrier.signal()
+        }
+        task.resume()
+        if !barrier.wait(until: Date().addingTimeInterval(timeout)) {
+            throw IncrementalWebServiceError.operationTimedOut
+        }
+        if let error = foundError {
+            throw error
+        }
+    }
+    
+    //
+    // MARK: - Methods to be overriden
+    //
+    
+    open func getUniqueIds(for fetchRequest: NSFetchRequest<NSFetchRequestResult>) throws -> [Any] {
+        throw IncrementalWebServiceError.mustBeOverriden
+    }
+    
+    open func getValuesForObject(using key: Any) throws -> [String : Any] {
+        throw IncrementalWebServiceError.mustBeOverriden
+    }
+    
+    open func createKeys(for objects: [NSManagedObject]) throws -> [(Any, NSManagedObject)] {
+        throw IncrementalWebServiceError.mustBeOverriden
+    }
+    
+    open func persistToBackingStore(saveRequest: NSSaveChangesRequest, with context: NSManagedObjectContext?) throws {
+        throw IncrementalWebServiceError.mustBeOverriden
+    }
+}
+
+//
+// MARK: - Hashable & Equatable conformance
+//
+
+extension IncrementalWebService: Hashable {
+    public var hashValue: Int {
+        return entity.hashValue
+    }
+    
+    public static func == (a: IncrementalWebService, b: IncrementalWebService) -> Bool {
+        return a.hashValue == b.hashValue
+    }
 }
