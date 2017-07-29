223153c50c075ded13f9f1d43651a3e840d6f367
Ronaldo Faria Lima <ronaldo@nineteen.com.br>
2017-07-29 11:41:03 -0300

    Added documentation for the WebService code.

--- Skelletor/Data/WebService.swift
+++ Skelletor/Data/WebService.swift
@@ -8,17 +8,31 @@
 
 import Foundation
 
+/// Simple RESTful web service interface
 public class WebService {
     public let root: URL
     public var session: URLSession
     let apiKey: String
     
+    /// Initializer
+    ///
+    /// - Parameters:
+    ///   - root: web service ROOT URL
+    ///   - apiKey: apikey to be used with the web service.
+    ///   - session: Session to use.
     public init(root: URL, apiKey: String, session: URLSession = URLSession.shared) {
         self.root = root
         self.session = session
         self.apiKey = apiKey
     }
     
+    /// Build a proper request for the web service
+    ///
+    /// - Parameters:
+    ///   - resource: Resource to contact
+    ///   - timeout: Timeout. Defaults to 30 seconds.
+    ///   - configure: Callback used to customize the request
+    /// - Returns: Created request
     public func request(for resource: String, timeout: TimeInterval = 30, configure: ((URLRequest)->URLRequest)? = nil) -> URLRequest {
         let url = root.appendingPathComponent(resource)
         var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: timeout)
@@ -31,6 +45,12 @@ public class WebService {
         return request
     }
     
+    /// Executes an URLRequest using the internal session
+    ///
+    /// - Parameters:
+    ///   - request: request to execute
+    ///   - completionHandler: callback for data return and error processing
+    /// - Returns: Data task created, already running in background
     @discardableResult
     public func execute(request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
         let task = session.dataTask(with: request) { (data, response, error) in
